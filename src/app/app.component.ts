6dcb44bf16270a15f05245f5aa57ff7daa8c858d
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-11-02 21:07:06 +0200

    TD:Using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -28,7 +36,7 @@ export class AppComponent {
     // ============== patchValue ==> to overwrite parts of the form
     this.signupForm.form.patchValue({
       userData: {
-        userName: suggestedName
+        username: suggestedName
       }
     });
   }
@@ -37,6 +45,11 @@ export class AppComponent {
   //   console.log(form);
   // }
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
